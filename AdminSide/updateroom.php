043b58ccdb085671e325c96ba63f4e0b6605dce5
Jerry <jerrypunzalan24@gmail.com>
2018-01-17 22:11:12 +0800

    Fixed broken image after editing room w/o image upload

--- AdminSide/updateroom.php
+++ AdminSide/updateroom.php
@@ -8,7 +8,10 @@
     $roomRate = mysqli_escape_string($conn, $_POST['roomRate']);
     $roomNumber = mysqli_escape_string($conn, $_POST['roomNumber']);
     $roomStatus = mysqli_escape_string($conn, $_POST['roomStatus']);
-    $query = "UPDATE room_masterfile SET room_id=$roomId,room_type='$roomType',room_description='$roomDescription',room_capacity='$roomCapacity',room_rate=$roomRate,room_number=$roomNumber,room_status='$roomStatus', room_imagepath ='img/{$_POST['image_upload']}' WHERE room_id = $roomId";
+    $image_upload = '';
+    if($_POST['image_upload'] != '')
+      $image_upload = ",room_imagepath = 'img/{$_POST['image_upload']}'";
+    $query = "UPDATE room_masterfile SET room_id=$roomId,room_type='$roomType',room_description='$roomDescription',room_capacity='$roomCapacity',room_rate=$roomRate,room_number=$roomNumber,room_status='$roomStatus' {$image_upload} WHERE room_id = $roomId";
     try {
       $update_result = mysqli_query($conn,$query) or die (mysqli_error($conn));
       if ($update_result) {
