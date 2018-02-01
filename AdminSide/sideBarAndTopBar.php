8788dbdd1462ebccf655f4f888e7149fbaa71eeb
ALUmali <35109977+ALUmali@users.noreply.github.com>
2018-02-01 19:29:34 +0800

    Changed sidebar color

--- AdminSide/sideBarAndTopBar.php
+++ AdminSide/sideBarAndTopBar.php
@@ -30,17 +30,17 @@ session_start();
 
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.php">HomeTown Hotel Makati - Admin</a>
+  <nav class="navbar navbar-expand-lg navbar-dark  fixed-top" style ='background-color: white; ' id="mainNav">
+    <a class="navbar-brand" href="index.php" style ='color:gray'>HomeTown Hotel Makati - Admin</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
+      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion" style ='background-color:white;'>
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
           <a class="nav-link" href="index.php">
             <i class="fa fa-fw fa-dashboard"></i>
-            <span class="nav-link-text">Home</span>
+            <span class="nav-link-text" >Home</span>
           </a>
           <ul class="sidenav-second-level collapse" id="collapseExamplePages">
           </ul>
