53bd04a87757217101697ed5828e369adc1c0dee
Jerry <jerrypunzalan24@gmail.com>
2018-02-16 11:37:54 +0800

    can upload image anywhere in room

--- AdminSide/RoomsAdd.php
+++ AdminSide/RoomsAdd.php
@@ -12,7 +12,6 @@
 		$roomStatus = mysqli_escape_string($conn, $_POST['roomStatus']);
     $name = basename($_FILES['image_upload']['name']);
     $tmp = $_FILES['image_upload']['tmp_name'];
-    echo print_r($_POST). "<br/>". print_r($_FILES);
     move_uploaded_file($tmp,"../img/{$name}");
     $query = mysqli_query($conn, "INSERT INTO room_masterfile (room_type, room_description, room_capacity, room_rate, room_number, room_status, room_imagepath) VALUES('{$roomType}','{$roomDescription}',
        {$roomCapacity}, {$roomRate}, {$roomNumber},'{$roomStatus}','img/{$name}')") or die(mysqli_error($conn));
@@ -21,7 +20,7 @@
     for($i = 1; $i <= $roomNumber; $i++){
       mysqli_query($conn, "INSERT walkinrooms_masterfile(walkinrooms_name, room_id) VALUES('{$roomType}{$i}', {$room['max(room_id)']})") or die(mysqli_error($conn));
     }
-    echo "<script>window.alert('Success! Room added.');window.location.href='Roomsdelete.php';</script>";
+    // echo "<script>window.alert('Success! Room added.');window.location.href='Roomsdelete.php';</script>";
 // $insert_result = mysqli_query($conn, "INSERT INTO room_masterfile(room_type, room_description, room_capacity, room_rate, room_number, room_status, room_imagepath) 
     // VALUES ('{$roomType}', '{$roomDescription}', {$roomCapacity}, {$roomRate}, {$roomNumber}, '{$roomStatus}', '../img/{$roomImage}')") or die (mysqli_error($conn));
 	}
@@ -50,26 +49,26 @@
     <div class="card card-login mx-auto mt-5">
       <div class="card-header">Add Rooms</div>
       <div class="card-body">
-        <form method = "POST" action = "RoomsAdd.php" id = "addRoomsForm" enctype = 'multiform/form-data'>
+        <form method = "POST" enctype="multipart/form-data">
           <div class="form-group">
             <label for="RoomType">Room Type</label><br>
-            <input required class="form-control" name = "roomType" type="text" aria-describedby="emailHelp" placeholder="Room Type">
+            <input required required class="form-control" name = "roomType" type="text" aria-describedby="emailHelp" placeholder="Room Type">
           </div>
           <div class="form-group">
             <label for="roomDescription">Room Description</label><br>
-            <textarea rows="4" cols="47" name="roomDescription" form="addRoomsForm" placeholder="Enter room description here..."></textarea>
+            <textarea required rows="4" cols="47" name="roomDescription" form="addRoomsForm" placeholder="Enter room description here..."></textarea>
           </div>
           <div class="form-group">
             <label for="roomRate">Room Capacity</label><br>
-            <input required class="form-control" name = "roomCapacity" type="number" placeholder="Room Capacity">
+            <input required required class="form-control" name = "roomCapacity" type="number" placeholder="Room Capacity">
           </div>
           <div class="form-group">
             <label for="roomRate">Room Rate per night</label><br>
-            <input required class="form-control" name = "roomRate" type="number" placeholder="Room Rate">
+            <input required required class="form-control" name = "roomRate" type="number" placeholder="Room Rate">
           </div>
           <div class="form-group">
             <label for="roomNumber">Room Number</label><br>
-            <input required class="form-control" name = "roomNumber" type="number" placeholder="Room Number">
+            <input required required class="form-control" name = "roomNumber" type="number" placeholder="Room Number">
           </div>
            <div class="form-group">
             <label for="roomStatus">Room Status &nbsp&nbsp</label>
@@ -81,7 +80,7 @@
           </div>
           <div class='form-group'>
             <label for ='image_upload'>Image</label><br>
-            <input type ='file' accept = 'image/*' name = 'image_upload'>
+            <input required type ='file' name = 'image_upload'>
           </div>
            </div>
           <input type = 'submit' name = "submit" class="btn btn-primary btn-block" />
